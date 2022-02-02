06aa2c58d8938c578b67f2d8ba1f982fcd326f63
Axel Havukangas <axel@havukangas.fi>
2022-02-02 20:21:28 +0200

    Update dependencies (#71)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,8 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
-  rules: {
-    "@typescript-eslint/no-use-before-define": "off",
-    // Following rules have issues with async generators
-    "@typescript-eslint/await-thenable": "off",
-    "@typescript-eslint/require-await": "off",
-  }
+    "prettier"
+  ]
 };
